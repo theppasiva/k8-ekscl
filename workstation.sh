3d0fa8185a4bfa86428295d16ec4d165434ca096
theppasiva <theppasiva@gmail.com>
2024-06-01 01:56:43 +0530

    added helm installation steps

--- workstation.sh
+++ workstation.sh
@@ -64,3 +64,8 @@ VALIDATE $? "eksctl installation"
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+VALIDATE $? "helm installation"
